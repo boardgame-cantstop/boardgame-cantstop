c240debf46bac7fc558319a447b0e492c24e46c5
mestihudson <mestihudson@gmail.com>
2020-05-26 22:39:27 -0300

    chore(ci): create process to register version and build number of app on title of main page

--- app/config.sh
+++ app/config.sh
@@ -4,8 +4,12 @@ function dev() {
 }
 
 function ci() {
+  which jq || apk update && apk add jq
   yarn install && yarn build
+  sed "s|@VERSION@|$(cat package.json|jq -r .version)|g" -i build/index.html
+  sed "s|@BUILD@|$1|g" -i build/index.html
 }
 
 main="${1:-dev}"
-$main
+build="${2:-SNAPSHOT}"
+$main $build
